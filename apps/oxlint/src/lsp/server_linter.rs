47761aabfc7371ac563b561c086055c20c66e32e
Sysix <sysix@sysix-coding.de>
2026-01-10 00:07:09 +0100

    feat(oxlint/lsp): support `jsPlugins`

--- apps/oxlint/src/lsp/server_linter.rs
+++ apps/oxlint/src/lsp/server_linter.rs
@@ -44,6 +44,10 @@ pub struct ServerLinterBuilder {
 }
 
 impl ServerLinterBuilder {
+    pub fn new(external_linter: Option<ExternalLinter>) -> Self {
+        Self { external_linter }
+    }
+
     /// # Panics
     /// Panics if the root URI cannot be converted to a file path.
     pub fn build(&self, root_uri: &Uri, options: serde_json::Value) -> ServerLinter {
@@ -116,7 +120,7 @@ impl ServerLinterBuilder {
         extended_paths.extend(config_builder.extended_paths.clone());
         let base_config = config_builder.build(&mut external_plugin_store).unwrap_or_else(|err| {
             warn!("Failed to build config: {err}");
-            ConfigStoreBuilder::empty().build(&mut external_plugin_store).unwrap()
+            ConfigStoreBuilder::empty().build(&mut ExternalPluginStore::new(false)).unwrap()
         });
 
         let lint_options = LintOptions {
@@ -128,6 +132,8 @@ impl ServerLinterBuilder {
             },
             ..Default::default()
         };
+        let is_external_store_empty: bool = external_plugin_store.is_empty();
+
         let config_store = ConfigStore::new(
             base_config,
             if use_nested_config {
@@ -142,9 +148,21 @@ impl ServerLinterBuilder {
             external_plugin_store,
         );
 
+        // Send JS plugins config to JS side
+        if let Some(external_linter) = &self.external_linter {
+            let res = config_store
+                .external_plugin_store()
+                .setup_rule_configs(root_path.to_string_lossy().into_owned(), external_linter);
+            if let Err(err) = res {
+                error!("Failed to setup JS plugins config:\n{err}\n");
+            }
+        }
+
         let isolated_linter = IsolatedLintHandler::new(
             lint_options,
             config_store,
+            // If no external rules, discard `ExternalLinter`
+            if is_external_store_empty { None } else { self.external_linter.clone() },
             &IsolatedLintHandlerOptions {
                 use_cross_module,
                 type_aware: options.type_aware,
@@ -294,20 +312,26 @@ impl ServerLinterBuilder {
             };
             // Collect ignore patterns and their root
             nested_ignore_patterns.push((oxlintrc.ignore_patterns.clone(), dir_path.to_path_buf()));
-            let Ok(config_store_builder) = ConfigStoreBuilder::from_oxlintrc(
+            let config_store_builder = match ConfigStoreBuilder::from_oxlintrc(
                 false,
                 oxlintrc,
                 self.external_linter.as_ref(),
                 external_plugin_store,
-            ) else {
-                warn!("Skipping config (builder failed): {}", file_path.display());
-                continue;
+            ) {
+                Ok(builder) => builder,
+                Err(err) => {
+                    warn!("Skipping config: {}: {err}", file_path.display());
+                    continue;
+                }
             };
             extended_paths.extend(config_store_builder.extended_paths.clone());
-            let config = config_store_builder.build(external_plugin_store).unwrap_or_else(|err| {
-                warn!("Failed to build nested config for {}: {:?}", dir_path.display(), err);
-                ConfigStoreBuilder::empty().build(external_plugin_store).unwrap()
-            });
+            let config = match config_store_builder.build(external_plugin_store) {
+                Ok(config) => config,
+                Err(err) => {
+                    warn!("Skipping config: {}: {err}", file_path.display());
+                    continue;
+                }
+            };
             nested_configs.pin().insert(dir_path.to_path_buf(), config);
         }
 
